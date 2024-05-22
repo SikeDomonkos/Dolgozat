e6ce166bd488750a103754f7918ba7b63ab7e8ca
Sike Domonkos <siked@kkszki.hu>
2024-05-22 14:33:12 +0200

    asd

---
+++ indexTest.py
@@ -0,0 +1,28 @@
+# imports
+from selenium import webdriver 
+from selenium.webdriver.common import By
+from selenium.webdriver.common.action_chains import actionChains
+import time
+# set driver
+
+# functions
+
+# test1: dupla kattintás után szerepel-e az "animation" class
+def test1():
+    elementOne = driver.find_element(By.ID,value = element-one)
+    elementOne.click() 
+time.sleep(1)
+# ha rámegy az egér, utána létezik-e box-shadow
+def test2():
+
+time.sleep(1)
+# kattintás után eltűnik-e
+def test3():
+
+time.sleep(1)
+# kattintás után a háttere zöld lesz-e
+def test4():
+
+time.sleep(1)
+# ha a 4. elemre egeret viszünk, hogy néz ki? (screenshot kell csak!)
+def test5():
