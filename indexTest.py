cfd826c3998d8434edc106140705d28441b0a3e3
Sike Domonkos <siked@kkszki.hu>
2024-05-22 14:34:54 +0200

    asdd

--- indexTest.py
+++ indexTest.py
@@ -14,15 +14,27 @@ def test1():
 time.sleep(1)
 # ha rámegy az egér, utána létezik-e box-shadow
 def test2():
+    def test1():
+    elementOne = driver.find_element(By.ID,value = element-two)
+    elementOne.mouseover() 
 
 time.sleep(1)
 # kattintás után eltűnik-e
 def test3():
+    def test1():
+    elementOne = driver.find_element(By.ID,value = element-three)
+    elementOne.click() 
 
 time.sleep(1)
 # kattintás után a háttere zöld lesz-e
 def test4():
+    def test1():
+    elementOne = driver.find_element(By.ID,value = element-four)
+    elementOne.click() 
 
 time.sleep(1)
 # ha a 4. elemre egeret viszünk, hogy néz ki? (screenshot kell csak!)
 def test5():
+    def test1():
+    elementOne = driver.find_element(By.ID,value = element-five)
+    elementOne.click() 
